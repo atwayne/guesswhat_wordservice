2afdd9fc19acd5b3fe70c2bf93c0e82c2e49f61b
Wayne Wang <lastr2d2@gmail.com>
2015-06-25 03:57:57 +0800

    adding scheme.sql

---
+++ WayneStudio.WordService/WayneStudio.WordService/Controllers/WordController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace WayneStudio.WordService.Controllers
+{
+    public class WordController : AppController
+    {
+        [HttpGet]
+        [Route("wordlist")]
+        public HttpResponseMessage List()
+        {
+            
+        }
+    }
+}
