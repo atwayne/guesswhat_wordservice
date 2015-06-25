8aec0a40a1706111bc59accc6fba3e7f413ff685
Wayne Wang <lastr2d2@gmail.com>
2015-06-25 16:22:31 +0800

    api

--- WayneStudio.WordService/WayneStudio.WordService/Controllers/WordController.cs
+++ WayneStudio.WordService/WayneStudio.WordService/Controllers/WordController.cs
@@ -1,19 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
-using System.Web;
 using System.Web.Http;
+using WayneStudio.WordService.Models;
 
 namespace WayneStudio.WordService.Controllers
 {
     public class WordController : AppController
     {
+        [HttpPost]
+        [Route("word/create")]
+        public HttpResponseMessage Create(UpdateWordRequest request)
+        {
+            // TODO: save to database
+            return Request.CreateResponse(HttpStatusCode.Created);
+        }
+
         [HttpGet]
-        [Route("wordlist")]
+        [Route("word/list")]
         public HttpResponseMessage List()
         {
-            
+            // TODO: read from database
+            var wordList = new List<string>() { "foo", "bar" };
+            return Request.CreateResponse(HttpStatusCode.OK, wordList);
+        }
+
+
+        [HttpPost]
+        [Route("word/expire")]
+        public HttpResponseMessage List(UpdateWordRequest request)
+        {
+            // TODO: expire record
+            return Request.CreateResponse(HttpStatusCode.Created);
+        }
+
+        [HttpPost]
+        [Route("word/block")]
+        public HttpResponseMessage List(UpdateWordRequest request)
+        {
+            // TODO: block record
+            return Request.CreateResponse(HttpStatusCode.Created);
         }
     }
 }
---
+++ WayneStudio.WordService/WayneStudio.WordService/Models/UpdateWordRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace WayneStudio.WordService.Models
+{
+    public class UpdateWordRequest
+    {
+        public string CreatedBy { get; set; }
+        public List<string> Words { get; set; }
+    }
+}
