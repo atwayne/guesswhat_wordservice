e1c6d50f33af01abc2af517d00d05e53182582a8
Wayne Wang <lastr2d2@gmail.com>
2015-06-25 17:40:24 +0800

    create action

--- WayneStudio.WordService/WayneStudio.WordService/DataStore/WordEngine.cs
+++ WayneStudio.WordService/WayneStudio.WordService/DataStore/WordEngine.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Collections.Generic;
 using WayneStudio.WordService.Core;
 using WayneStudio.WordService.Models;
+using System.Data.SQLite;
 
 namespace WayneStudio.WordService.DataStore
 {
@@ -11,7 +12,29 @@ namespace WayneStudio.WordService.DataStore
     {
         public void AddWords(UpdateWordRequest request)
         {
-            throw new NotImplementedException();
+            var query = @"
+                INSERT INTO Word (Word, CreatedBy)
+                VALUES (@Word,@CreatedBy)";
+
+            var existed = GetAllWords()
+                .Select(l => l.Text).ToList();
+
+            var wordsToBeSaved = request.Words
+            .Where(l => !existed.Contains(l, StringComparer.OrdinalIgnoreCase))
+            .ToList();
+
+            foreach (var item in wordsToBeSaved)
+            {
+                var parameters = new List<SQLiteParameter>();
+                parameters.Add(new SQLiteParameter("Word", item));
+                parameters.Add(new SQLiteParameter("CreatedBy", request.CreatedBy));
+
+                try
+                {
+                    SQLiteHelper.ExecuteNonQuery(query, parameters.ToArray());
+                }
+                catch (SQLiteException) { }
+            }
         }
 
         public List<Word> GetWordList()
@@ -20,6 +43,21 @@ namespace WayneStudio.WordService.DataStore
                 SELECT Word AS Text, CreatedBy
                 FROM Word
                 WHERE IsBlocked = 0 AND IsExpired = 0";
+
+            return GetWordListByQuery(query);
+        }
+
+        private List<Word> GetAllWords()
+        {
+            var query = @"
+                SELECT Word AS Text, CreatedBy
+                FROM Word";
+
+            return GetWordListByQuery(query);
+        }
+
+        private static List<Word> GetWordListByQuery(string query)
+        {
             var queryResult = SQLiteHelper.ExecuteDataTable(query);
             return queryResult.AsEnumerable()
                 .Select(l =>
